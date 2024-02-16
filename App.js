af44163241fcab6d35e9efbfcf7e0f70cb4e390f
Aryainguz <aryaninguz369@gmail.com>
2024-02-16 10:45:35 +0530

    Initialized react native app with expo

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import HomeScreen from './app/navigation/HomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+      <HomeScreen/>
       <StatusBar style="auto" />
+      </NavigationContainer>
     </View>
   );
 }
@@ -14,7 +18,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
